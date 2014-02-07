6490b46683ec9a049f631a5e5d83aa6e9c888952
MapR <mapr@vestibule.local>
2014-02-07 00:44:34 -0800

    cleanup for upload to CRAN

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-R CMD build Storm
-R CMD check Storm
-cat Storm.Rcheck/00install.out
+# Write Storm Bolts in R using the Storm Multi-Language Protocol - *R Support for Apache Storm Multilang Protocol*
+
+## Description
+
+Storm is a distributed realtime computation system. Similar to how Hadoop provides a set of general primitives for doing batch processing, Storm provides a set of general primitives for doing realtime computation.
+
+Storm includes a “Multi-Language” (or “Multilang”) Protocol to allow implementation of Bolts and Spouts in languages other than Java. This R extension provides implementations of utility functions to allow an application developer to focus on application-specific functionality rather than Storm/R communications plumbing.
+
+## Details
+
+From Storm’s point of view, it creates an R process to consume and produce Tuples. Storm commu- nicates with R using a JSON-like format. Storm writes Tuples via STDIN, and reads Tuples from R via STDOUT. The Storm package implements several functions to take care of Storm/R I/O.
+
+As the application programmer, you implement a single function with signature:v “function(s=Storm, t=Tuple)” that will be called once per Tuple. Inside this function, you can emit zero or more Tuples, as well as emit other status messages, such as failures and diagnostic messages.
+
+To use this extension, briefly:
+
+1. create a new Storm object.
+2. define a function that can process and emit Tuple objects.
+3. call the run() method on the Storm object.
+
+A detailed example is given in the examples section.
+
+## Git Repository Structure
+
+You'll find two directories here:
+
+* java/  - contains a maven project for testing R with a very simple string permutation test.  Run it as 'mvn test'.  You'll need to have the "Storm" and "permute" R packages installed.
+* Storm/ - contains the R Storm package itself.  To use it, you'll either want to install from CRAN (e.g. 'install.packages("Storm")'), or by 'R CMD install Storm'.
+
+## See Also
+[https://github.com/nathanmarz/storm/wiki/Using-non-JVM-languages-with-Storm](https://github.com/nathanmarz/storm/wiki/Using-non-JVM-languages-with-Storm)
+[https://github.com/nathanmarz/storm/wiki/Multilang-protocol](https://github.com/nathanmarz/storm/wiki/Multilang-protocol)
+
--- Storm/R/Storm.R
+++ Storm/R/Storm.R
@@ -67,9 +67,21 @@ Storm$methods(
 Storm$methods(
   run = function() {
     buf = "";
+
+    x.stdin = file("stdin");
+    open(x.stdin);
+
+    #x.stdout = file("stdout");
+    #cat(paste('{"pid": ',Sys.getpid(),'}',"\n",sep=""),file=x.stdout);
+    cat(paste('{"pid": ',Sys.getpid(),'}',"\nend\n",sep=""));
+    cat('{"command": "emit", "anchors": [], "tuple": ["bolt initializing"]}\nend\n');
+    #flush(x.stdout);
+    #close(x.stdout);
+
     while (TRUE) {
-      rl = as.character(readLines(n=1));
+      rl = as.character(readLines(con=x.stdin,n=1,warn=FALSE));
       if (length(rl) == 0) {
+        close(x.stdin);
         break;
       }
       if (rl == "end" || rl == "END") {
@@ -100,11 +112,11 @@ Storm$methods(
     # we assume that it is a setup if the "pidDir" field is not empty.
     else if (!is.null(t$pidDir)) {
       .self$setup = t;
-      .self$process_setup();
+      .self$process_setup(t);
     }
     # otherwise, not sure what it is.  log an error and stop.
     else {
-      .self$log("unrecognized JSON: ",json);
+#TODO      .self$log(paste("unrecognized JSON:\n'",json,"'\n",sep=""));
       #stop("unrecognized JSON: ",json);
     }
   }
@@ -122,7 +134,8 @@ Storm$methods(
   }
 );
 Storm$methods(
-  process_setup = function() {
+  process_setup = function(json=character) {
+    file.create(paste(json$pidDir,"/",Sys.getpid(),sep=""))
     #    print(.self$input.setup);
   }
 );
@@ -130,9 +143,9 @@ Storm$methods(
 Storm$methods(
   log = function(msg=character) {
     cat(c(
-      '{\n',
-      '\t"command": "log",\n',
-      '\t"msg": "',msg,'"\n',
+      '{',
+      '"command": "log",',
+      '"msg": "',msg,'"',
       '}\n',
       'end\n'
     ),sep="");
@@ -140,21 +153,24 @@ Storm$methods(
 );
 Storm$methods(
   ack = function(tuple=Tuple) {
+    x.stdout = file("stdout");
     cat(c(
       '{\n',
       '\t"command": "ack",\n',
       '\t"id": "',tuple$id,'"\n',
       '}\n',
       'end\n'
-    ),sep="");
+    ),sep="",file=x.stdout);
+    flush(x.stdout);
+    close(x.stdout);
   }
 );
 Storm$methods(
   fail = function(tuple=Tuple) {
     cat(c(
-      '{\n',
-      '\t"command": "fail",\n',
-      '\t"id": "',tuple$id,'"\n',
+      '{',
+      '"command": "fail",',
+      '"id": "',tuple$id,'"',
       '}\n',
       'end\n'
     ),sep="") 
@@ -162,25 +178,39 @@ Storm$methods(
 );
 Storm$methods(
   emit = function(tuple=Tuple) {
-    t.prefix="";
-    t.suffix="";
-    if (length(tuple$output) == 1) {
-      t.prefix="[";
-      t.suffix="]";
-    }
-    a.prefix="";
-    a.suffix="";
-    if (length(tuple$anchors) == 1) {
-      a.prefix="[";
-      a.suffix="]";
-    }
+    t.prefix="[";
+    t.suffix="]";
+    #if (length(tuple$output) == 1) {
+    #  t.prefix="[";
+    #  t.suffix="]";
+    #}
+    a.prefix="[";
+    a.suffix="]";
+    #if (length(tuple$anchors) == 1) {
+    #  a.prefix="[";
+    #  a.suffix="]";
+    #}
     cat(c(
-      '{\n',
-      '\t"command": "emit",\n',
-      '\t"anchors": ',a.prefix,toJSON(tuple$anchors),a.suffix,',\n',
-      '\t"stream": "',tuple$stream,'",\n',
-      '\t"task": "',tuple$task,'",\n',
-      '\t"tuple": ',t.prefix,toJSON(tuple$output),t.suffix,',\n',
+      '{',
+      '"command": "emit", ',
+      
+      #TODO enable reliable tuples, see
+      #https://github.com/nathanmarz/storm/wiki/Guaranteeing-message-processing
+      #'"id": "',as.character(runif(1) * 2**64 - 1),'", ',
+      
+      #TODO enable multi-anchoring, see:
+      #https://github.com/nathanmarz/storm/wiki/Guaranteeing-message-processing      
+      '"anchors": ',a.prefix,toJSON(tuple$id),a.suffix,', ',
+
+      #TODO enable id of stream tuple is emitted to, see:
+      #https://github.com/nathanmarz/storm/wiki/Multilang-protocol
+      #'"stream": "',tuple$stream,'", ',
+
+      #TODO enable "emit direct", see:
+      #https://github.com/nathanmarz/storm/wiki/Multilang-protocol
+      #'"task": "',tuple$task,'", ',
+
+      '"tuple": ',t.prefix,toJSON(tuple$output),t.suffix,
       '}\n',
       'end\n'
     ),sep="");
